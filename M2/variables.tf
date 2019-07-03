ab26f60a668c36b850ce92b5102bb8129a422d27
olen2006 <olen2006@gmail.com>
2019-07-02 23:05:16 -0600

    Add files via upload

---
+++ M2/variables.tf
@@ -0,0 +1,13 @@
+############################
+#Variables
+############################
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
+
+variable "subnet_count" {
+  default = 2
+}
+
