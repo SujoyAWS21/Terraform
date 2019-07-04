6e838b4cc02031f43e24e735a7da349b20fc2f5e
Oleg <olen2006@gmail.com>
2019-07-03 23:05:29 -0600

    test

--- M2/variables.tf
+++ M2/variables.tf
@@ -1,13 +1,15 @@
 ############################
 #Variables
 ############################
-variable "aws_access_key" {
-}
-
-variable "aws_secret_key" {
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
 
 variable "subnet_count" {
   default = 2
 }
+variable "bucket_name"{}
+variable "environment_tag" {}
+variable "billing_code_tag" {}
+
 
