b5980b0cb6d1fd3ebc1131a48e057560a7112a5b
Oleg <olen2006@gmail.com>
2019-07-08 23:03:56 -0600

    networking2 for statelock test

---
+++ M4/networking2/variables.tf
@@ -0,0 +1,15 @@
+############################
+#Variables
+############################
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+
+variable "subnet_count" {
+  default = 1
+}
+variable "bucket_name"{}
+variable "environment_tag" {}
+variable "billing_code_tag" {}
+
+
